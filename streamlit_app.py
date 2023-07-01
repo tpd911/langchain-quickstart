b1666a9e6991a741009ebeb355b2836c0868c1f0
tpd911 <81883793+tpd911@users.noreply.github.com>
2023-07-01 15:07:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('🦜🔗 Quickstart App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
 with st.form('my_form'):
